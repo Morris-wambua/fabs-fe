42a0a9953b3a258570b88fe07d2744bcccb8a3b6
morris-wambua <morriswambua7@gmail.com>
2024-04-10 10:34:32 +0300

    fixed store import

--- src/app/store/store.service.ts
+++ src/app/store/store.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { Store } from '../models/store';
 import { environment } from 'src/environments/environment.development';
+import { Store } from './store';
 
 @Injectable({
   providedIn: 'root',

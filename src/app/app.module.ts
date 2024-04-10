04ed70df02b371463f21dce25eb743b65fb37a99
morris-wambua <morriswambua7@gmail.com>
2024-04-10 09:48:34 +0300

    add app components and module

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UserComponent } from './user/user.component';
+import { AppRoutingModule } from './app-rounting.module';
+import { UserService } from './services/user.service';
+import { LocationComponent } from './location/location.component';
+import { LocationService } from './services/location.service';
+import { StoreComponent } from './store/store.component';
+import { StoreService } from './services/store.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserComponent,
+    LocationComponent,
+    StoreComponent,
+  ],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  providers: [UserService, LocationService, StoreService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

f4045f4268de7d92afbcbf1904135f99e2d183fa
morris-wambua <morriswambua7@gmail.com>
2024-05-27 19:34:47 +0300

    Add dynamic fetching of type of reservation

--- src/app/reservation/reservation.component.ts
+++ src/app/reservation/reservation.component.ts
@@ -3,6 +3,12 @@ import { ReservationService } from './reservation.service';
 import { Reservation } from './Reservation';
 import { HttpErrorResponse } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TypeOfService } from '../general/TypeOfService';
+import { TypeofserviceService } from '../general/typeofservice.service';
+import { tap } from 'rxjs';
+import { StoreService } from '../store/store.service';
+import { Store } from '../store/store';
 
 @Component({
   selector: 'app-reservation',
@@ -15,17 +21,47 @@ export class ReservationComponent implements OnInit {
   public user!: string;
   public username!: string;
   public reservationToDelete: Reservation | null = null;
+  public addReservationForm!: FormGroup; // declare an empty form
+  typeOfServices: TypeOfService[] = [];
+  stores: Store[] = [];
+  availableTimes: string[] = [];
 
   constructor(
     private reservationService: ReservationService,
-    private router: Router
+    private tosService: TypeofserviceService,
+    private storeService: StoreService,
+    private router: Router,
+    private fb: FormBuilder
   ) {}
 
   ngOnInit(): void {
     this.fetchReservations();
     this.fetchLoginUser();
+    this.initializeForm();
+    this.fetchTypeOfServices();
+    this.onReservationTypeChange();
+    this.generateTimeOptions();
+    this.fetchStores();
   }
 
+  // This will initialize the add reservation form
+  private initializeForm(): void {
+    this.addReservationForm = this.fb.group({
+      id: ['', Validators.required],
+      name: ['', Validators.required],
+      price: [{ value: '', disabled: true }, Validators.required],
+      reservationDate: [''],
+      startTime: [''],
+      endTime: [''],
+      expert: ['', Validators.required],
+      store: ['', Validators.required],
+      typeOfService: ['', Validators.required],
+      reservationExpert: [{ value: '', disabled: true }, Validators.required],
+      status: [''],
+    });
+  }
+
+  // This controls any modals opening on the reservation page
   public onOpenModal(reservation: Reservation | any, mode: string): void {
     console.log('open modal clicked');
 
@@ -37,7 +73,7 @@ export class ReservationComponent implements OnInit {
     button.setAttribute('data-bs-toggle', 'modal');
 
     if (mode === 'add') {
-      button.setAttribute('data-bs-target', '#addUserModal');
+      button.setAttribute('data-bs-target', '#addReservationModal');
     }
     if (mode === 'edit') {
       button.setAttribute('data-bs-target', '#updateUserModal');
@@ -51,6 +87,7 @@ export class ReservationComponent implements OnInit {
     button.click();
   }
 
+  // Fetch all reservations
   public fetchReservations(): void {
     this.reservationService.getReservations().subscribe(
       (response: Reservation[]) => {
@@ -63,6 +100,7 @@ export class ReservationComponent implements OnInit {
     );
   }
 
+  // Delete reservation
   public deleteReservation(id: string | undefined): void {
     if (id) {
       this.reservationService.deleteReservation(id).subscribe(
@@ -81,10 +119,12 @@ export class ReservationComponent implements OnInit {
     }
   }
 
+  // Toggle side nav
   public toggleSideNav(): void {
     this.isSidebarVisible = !this.isSidebarVisible;
   }
 
+  // handle log out event
   public logout(): void {
     // Clear the auth token from local storage
     window.localStorage.removeItem('auth_token');
@@ -95,6 +135,7 @@ export class ReservationComponent implements OnInit {
     this.router.navigate(['']);
   }
 
+  // Fetch the loggedIn user
   public fetchLoginUser(): void {
     let fetchedUser = window.localStorage.getItem('name');
     let fetchedUsername = window.localStorage.getItem('username');
@@ -107,6 +148,63 @@ export class ReservationComponent implements OnInit {
       this.username = fetchedUsername;
     } else {
       this.username = 'john.doe@fabs.app';
+    }
+  }
+
+  // Fetch Type of services
+  public fetchTypeOfServices(): void {
+    this.tosService
+      .getAllServices()
+      .pipe(
+        tap((services: TypeOfService[]) => {
+          this.typeOfServices = services;
+        })
+      )
+      .subscribe({
+        error: (error) => {
+          console.error('Error fetching type of services:', error);
+        },
+      });
+  }
+
+  // This will dynamically update the price based on the user's choice of type of service
+  private onReservationTypeChange(): void {
+    this.addReservationForm
+      .get('name')
+      ?.valueChanges.subscribe((selectedName) => {
+        const selectedService = this.typeOfServices.find(
+          (service) => service.name === selectedName
+        );
+        if (selectedService) {
+          this.addReservationForm.get('price')?.setValue(selectedService.price);
+        } else {
+          this.addReservationForm.get('price')?.reset();
+        }
+      });
+  }
+
+  // Fetch Type of stores
+  public fetchStores(): void {
+    this.storeService
+      .getAllStores()
+      .pipe(
+        tap((stores: Store[]) => {
+          this.stores = stores;
+        })
+      )
+      .subscribe({
+        error: (error) => {
+          console.error('Error fetching stores:', error);
+        },
+      });
+  }
+
+  // Create chunks for available time
+  private generateTimeOptions(): void {
+    // Generate time options for a 24-hour clock
+    for (let i = 0; i < 24; i++) {
+      const time = `${i.toString().padStart(2, '0')}:00`; // Format as 'HH:00'
+      this.availableTimes.push(time);
     }
   }
 }
